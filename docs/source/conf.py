1560717d6617ac3263b8ef134fec8b462d356758
newworld-itp <119436272+newworld-itp@users.noreply.github.com>
2022-11-29 17:24:21 +0100

    Changed the project information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Newworld'
+copyright = '2022, Newworld'
+author = 'Newworld'
 
 release = '0.1'
 version = '0.1.0'
